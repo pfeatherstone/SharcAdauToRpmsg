3d69bf91777d32fee3148bd2c29263d3a91ba225
pfeatherstone <pfeatherstone@pf>
2024-05-12 14:57:26 +0100

    Sharc 2

---
+++ AdauToRpmsg_Core2/src/AdauToRpmsg_Core2.h
@@ -0,0 +1,11 @@
+/*****************************************************************************
+ * AdauToRpmsg_Core2.h
+ *****************************************************************************/
+
+#ifndef __ADAUTORPMSG_CORE2_H__
+#define __ADAUTORPMSG_CORE2_H__
+
+
+/* Add your custom header content here */
+
+#endif /* __ADAUTORPMSG_CORE2_H__ */
