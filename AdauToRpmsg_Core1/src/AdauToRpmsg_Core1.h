72350bf4debab017c4445bdbabb4a5efbbd3c5b4
pfeatherstone <pfeatherstone@pf>
2024-05-12 14:56:57 +0100

    Sharc 1

---
+++ AdauToRpmsg_Core1/src/AdauToRpmsg_Core1.h
@@ -0,0 +1,11 @@
+/*****************************************************************************
+ * AdauToRpmsg_Core1.h
+ *****************************************************************************/
+
+#ifndef __ADAUTORPMSG_CORE1_H__
+#define __ADAUTORPMSG_CORE1_H__
+
+
+/* Add your custom header content here */
+
+#endif /* __ADAUTORPMSG_CORE1_H__ */
